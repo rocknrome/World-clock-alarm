cdd40f352b9b945e88f01e13b83c89c833cc5921
Roman Larionov <126816805+rocknrome@users.noreply.github.com>
2023-12-02 15:02:23 -0500

    Setting up the functionality structure of app.js file

--- app.js
+++ app.js
@@ -1,4 +1,36 @@
 //   R'N'R   November 29, 2023
 
-const $input = $("input");
-console.log($input);
+//---------------------------------------------------//
+//              APP JAVASCRIPT FILE                  //
+//---------------------------------------------------//
+
+
+    //---------------------------------------------------//
+    //Declaring variables
+    //---------------------------------------------------//
+    const $timezoneSelect = $('#timezone-select');
+    const $alarmTimeInput = $('#alarm-time');
+    const $setAlarmButton = $('#set-alarm');
+    const $alarmDisplay = $('#alarm-display');
+    let alarms = [];
+
+
+    //---------------------------------------------------//
+    //Setting up functionality
+    //---------------------------------------------------//
+
+    // Fetch and populate timezones
+
+
+    // Function to display alarms
+
+
+    // Set an alarm
+
+
+    // Edit an alarm
+
+
+    // Delete an alarm
+
+
