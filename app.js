dcbde0483f06f74602a2065857af637192f811fe
Roman Larionov <126816805+rocknrome@users.noreply.github.com>
2023-12-04 20:04:24 -0500

    Edit button functionality added

--- app.js
+++ app.js
@@ -35,6 +35,7 @@
     }
     populateTimeZone()
 
+
     // Function to display alarms
     function displayAlarms() {
         $alarmDisplay.empty();  //clearing out the display block initially
@@ -50,6 +51,7 @@
       }
     //   displayAlarms()
 
+
     // Set an alarm
     $setAlarmButton.click(function() {  //event listener using the jQuery method ".click"
         const timezone = $timezoneSelect.val();
@@ -58,16 +60,26 @@
         displayAlarms();
       });
 
+
     // Edit an alarm
-    window.editAlarm = function(index) {
         //edit functionality here
-    }
+        window.editAlarm = function(index) {
+            const newTime = prompt("Enter new time (HH:MM format):", alarms[index].time);   //declaring new variables for adjusted time
+                if(newTime) {
+                    alarms[index].time = newTime;
+                    displayAlarms();
+                }
+        };
+
 
     // Delete an alarm
-    window.deleteAlarm = function(index) {
+    window.deleteAlarm = function(index) {  //declaring a global function thru "window"
         alarms.splice(index, 1);    //removing specific alarm with a specific index, one element only
         displayAlarms();
     };
+
+
+    // Alarm triggering
 
 
 
